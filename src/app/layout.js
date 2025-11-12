25aed4303fc52558d7d5a5aa50c36dabd3e633a6
alwannugrahaputra-lgtm <alwannugrahaputra@gmail.com>
2025-11-12 18:57:25 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <body className="bg-gradient-to-br from-teal-500 to-cyan-600 min-h-screen text-gray-900 dark:text-gray-100 relative">
+      <body className="bg-gradient-to-br from-teal-500 via-cyan-500 to-blue-500 min-h-screen text-gray-900 dark:text-gray-100 relative">
         <ThemeToggle />
         <main className="max-w-3xl mx-auto py-10 px-4">{children}</main>
       </body>
