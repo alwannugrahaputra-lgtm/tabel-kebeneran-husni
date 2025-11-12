fb2b949b0099796e636b2e83fa870900551e106d
alwannugrahaputra-lgtm <alwannugrahaputra@gmail.com>
2025-11-12 18:28:19 +0700

    Create layout.js
    Add layout.js to fix Next.js root layout requirement

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Kalkulator Tabel Kebenaran",
+  description: "Dibuat oleh Rifani Husni Mubarok 2025",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
