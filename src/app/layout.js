4e61ae3fed6f0e3cb6953b556bcc8d0b1e1073df
alwannugrahaputra-lgtm <alwannugrahaputra@gmail.com>
2025-11-12 19:18:35 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,14 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
   title: "Kalkulator Tabel Kebenaran",
   description: "Dibuat oleh Rifani Husni Mubarok 2025",
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id" className="dark">
       <body>{children}</body>
     </html>
-  )
+  );
 }
